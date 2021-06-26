ede80a8666f399652325601a849accbfb161c406
zxiong89 <zxiong89@gmail.com>
2021-06-26 10:45:19 -0500

    Unit Skeleton

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    #region Stat blocks
+    [SerializeField]
+    private StrengthStat Str;
+
+    [SerializeField]
+    private ConstitutionStat Con;
+
+    [SerializeField]
+    private DexterityStat Dex;
+
+    [SerializeField]
+    private IntelligenceStat Int;
+
+    [SerializeField]
+    private WisdomStat Wis;
+    #endregion
+
+    #region Methods
+    #endregion 
+
+    #region MonoBehavior
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+}
