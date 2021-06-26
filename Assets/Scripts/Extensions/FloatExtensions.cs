9506f210c0fd7527651851c1b267919ebb9c0414
zxiong89 <zxiong89@gmail.com>
2021-06-26 13:35:00 -0500

    Update with Gaussian Randomize for Floats

--- Assets/Scripts/Extensions/FloatExtensions.cs
+++ Assets/Scripts/Extensions/FloatExtensions.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
 
 public static class FloatExtensions
 {
+    /// <summary>
+    /// Returns the average of a number of float variables
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
     public static float Average(params float[] args)
     {
         double sum = 0;
@@ -12,5 +15,26 @@ public static class FloatExtensions
             sum += f;
         }
         return (float)(sum / args.Length);
+    }
+
+    /// <summary>
+    /// Using a guassian distrubution, get a random number.
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static float Randomize(float min, float max)
+    {
+        float mean = (min + max) / 2f;
+        float stdDev = (max - min) / 2f;
+        Random rand = new Random(); 
+        double u1 = 1.0 - rand.NextDouble(); 
+        double u2 = 1.0 - rand.NextDouble();
+        double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                     Math.Sin(2.0 * Math.PI * u2); 
+        double randNormal =
+                     mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+
+        return (float)randNormal;
     }
 }
