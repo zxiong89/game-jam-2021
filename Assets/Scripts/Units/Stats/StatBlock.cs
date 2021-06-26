70f66dc6ccc467f9626b0b64985ed73b21f30a7b
zxiong89 <zxiong89@gmail.com>
2021-06-26 12:23:13 -0500

    Abstract to StatBlock and Update values on Level change

---
+++ Assets/Scripts/Units/Stats/StatBlock.cs
@@ -0,0 +1,21 @@
+﻿public class StatBlock
+{
+    public StrengthStat Str;
+
+    public ConstitutionStat Con;
+
+    public DexterityStat Dex;
+
+    public IntelligenceStat Int;
+
+    public WisdomStat Wis;
+
+    public void UpdateStats(int level)
+    {
+        Str.Update(level);
+        Con.Update(level);
+        Dex.Update(level);
+        Int.Update(level);
+        Wis.Update(level);
+    }
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    #region Stat blocks
+    #region Fields/Properties
+    private int level;
+    public int Level
+    {
+        get => level;
+        set
+        {
+            level = value;
+            Stats?.UpdateStats(level);
+        }
+    }
+
+    public StatBlock Stats;
+    #endregion
+
+    #region Growth Limits
     [SerializeField]
-    private StrengthStat Str;
+    private GrowthFactor maxGrowth;
 
     [SerializeField]
-    private ConstitutionStat Con;
-
-    [SerializeField]
-    private DexterityStat Dex;
-
-    [SerializeField]
-    private IntelligenceStat Int;
-
-    [SerializeField]
-    private WisdomStat Wis;
+    private GrowthFactor minGrowth;
     #endregion
 
     #region Methods
+    public void LevelUp() => Level++;
     #endregion 
 
     #region MonoBehavior
