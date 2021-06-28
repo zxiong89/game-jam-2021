8a0d4f32df3a51c7199adc8d12d8533d795b274f
NullInference <nullinference@yahoo.com>
2021-06-28 16:39:36 -0700

    UI for unit recruitment

---
+++ Assets/Scripts/UI/UnitResumeDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UnitResumeDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    [SerializeField]
+    private StatsGroup statsGroup;
+
+    [SerializeField]
+    private TextMeshProUGUI traits;
+
+    [SerializeField]
+    private TextMeshProUGUI fee;
+
+    private void SetResume()
+    {
+
+    }
+}
