13a0fa0c954e4381ca120426cdbd1c5d662e0e86
NullInference <nullinference@yahoo.com>
2021-06-27 17:20:57 -0700

    Added display for traits and a more detailed unit summary

---
+++ Assets/Scripts/UI/TraitDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TraitDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI traitName;
+
+    [SerializeField]
+    private TextMeshProUGUI traitDescription;
+
+    public void SetTrait(string name, string description, Color textColor)
+    {
+        traitName.text = name;
+        traitName.color = textColor;
+        traitDescription.text = description;
+    }
+}
---
+++ Assets/Scripts/UI/TraitsGroup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TraitsGroup : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject traitsContainer;
+
+    [SerializeField]
+    private GameObject traitsPrefab;
+
+    public void AddTrait(string name, string description, Color color)
+    {
+        GameObject newTraitObj = Instantiate(traitsPrefab, traitsContainer.transform);
+        var newTrait = newTraitObj.GetComponent<TraitDisplay>();
+        newTrait.SetTrait(name, description, color);
+    }
+}
