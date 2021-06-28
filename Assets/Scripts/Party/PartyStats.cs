60f17c6372ff20a10b670da412b444aac8015fdc
zxiong89 <zxiong89@gmail.com>
2021-06-28 14:17:57 -0500

    Override +/- operators for PartyStats

--- Assets/Scripts/Party/PartyStats.cs
+++ Assets/Scripts/Party/PartyStats.cs
@@ -6,4 +6,49 @@ public struct PartyStats
     public float Def;
     public float AtkSup;
     public float DefSup;
+
+    public PartyStats(PartyStats copy)
+    {
+        PhyAtk = copy.PhyAtk;
+        MagAtk = copy.MagAtk;
+        Def = copy.Def;
+        AtkSup = copy.AtkSup;
+        DefSup = copy.DefSup;
+    }
+
+    public static PartyStats operator +(PartyStats a) => a;
+    public static PartyStats operator +(PartyStats a, PartyStats b)
+    {
+        return new PartyStats
+        {
+            PhyAtk = a.PhyAtk + b.PhyAtk,
+            MagAtk = a.MagAtk + b.MagAtk,
+            Def = a.Def + b.Def,
+            AtkSup = a.AtkSup + b.AtkSup,
+            DefSup = a.DefSup + b.DefSup
+        };
+    }
+    public static PartyStats operator -(PartyStats a)
+    {
+        return new PartyStats
+        {
+            PhyAtk = -a.PhyAtk,
+            MagAtk = -a.MagAtk,
+            Def = -a.Def,
+            AtkSup = -a.AtkSup,
+            DefSup = -a.DefSup
+        };
+    }
+
+    public static PartyStats operator -(PartyStats a, PartyStats b)
+    {
+        return new PartyStats
+        {
+            PhyAtk = a.PhyAtk - b.PhyAtk,
+            MagAtk = a.MagAtk - b.MagAtk,
+            Def = a.Def - b.Def,
+            AtkSup = a.AtkSup - b.AtkSup,
+            DefSup = a.DefSup - b.DefSup
+        };
+    }
 }
