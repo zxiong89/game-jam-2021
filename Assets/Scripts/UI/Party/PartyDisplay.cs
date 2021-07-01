c12507eaeb24c266a87da2d19918877f5126d0ec
zxiong89 <zxiong89@gmail.com>
2021-06-30 19:16:39 -0500

    Party UI Scripts

---
+++ Assets/Scripts/UI/Party/PartyDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PartyDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI nameDisplay;
+
+    [SerializeField]
+    private PartyStatsGroup stats;
+
+    public Party CurrentParty { get; private set; }
+
+    public void DisplayParty(Party party)
+    {
+        CurrentParty = party;
+        if (nameDisplay != null) nameDisplay.text = CurrentParty.Name;
+        stats?.DisplayPartyStats(party.Stats);
+    }
+}
---
+++ Assets/Scripts/UI/Party/PartyStatsGroup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyStatsGroup : MonoBehaviour
+{
+    [SerializeField]
+    private LabelValueDisplay[] displays;
+
+    public void DisplayPartyStats(PartyStats stats) 
+    {
+        if (displays == null || displays.Length != 6) return;
+
+        displaySinglePartyStat(displays[0], nameof(stats.Atk), stats.Atk);
+        displaySinglePartyStat(displays[1], nameof(stats.PhyAtk), stats.PhyAtk);
+        displaySinglePartyStat(displays[2], nameof(stats.MagAtk), stats.MagAtk);
+        displaySinglePartyStat(displays[3], nameof(stats.Def), stats.Def);
+        displaySinglePartyStat(displays[4], nameof(stats.AtkSup), stats.AtkSup);
+        displaySinglePartyStat(displays[5], nameof(stats.DefSup), stats.DefSup);
+    } 
+
+    private void displaySinglePartyStat(LabelValueDisplay display, string label, float value) => display.SetLabelValue(label, FloatExtensions.ToString(value));
+}
