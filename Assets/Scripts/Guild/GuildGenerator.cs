1bba7594afccd9db1548781e0a3bfa7953b53093
zxiong89 <zxiong89@gmail.com>
2021-07-01 18:09:57 -0500

    Create GuildGenerator for Testing

---
+++ Assets/Scripts/Guild/GuildGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuildGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Guild guild;
+
+    [SerializeField]
+    private UnitFactory factory;
+
+    public void GenerateGuild()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            guild.Roster.Add(factory.RandomizeUnit());
+        }
+    }
+}
