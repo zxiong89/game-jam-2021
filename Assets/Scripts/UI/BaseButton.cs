4a5a68b0427222fa8811bfecee15ce58f4510f56
NullInference <nullinference@yahoo.com>
2021-07-01 17:13:25 -0700

    Made some standard buttons

---
+++ Assets/Scripts/UI/BaseButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Class to make it easier to manipulate buttons
+/// </summary>
+public class BaseButton : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI buttonText;
+
+    [SerializeField]
+    private Button buttonComponent;
+
+    public void SetText(string newText)
+    {
+        buttonText.text = newText;  
+    }
+
+    public void SetCallback(Action callback)
+    {
+        buttonComponent.onClick.AddListener(() => callback.Invoke());
+    }
+}
