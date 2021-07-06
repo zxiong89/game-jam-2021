262863bd5509f0b9042576b802732cc3201e5843
zxiong89 <zxiong89@gmail.com>
2021-07-06 18:27:50 -0500

    Add in PartyPanel

---
+++ Assets/Scripts/UI/Party/PartyPanel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyPanel : MonoBehaviour
+{
+    [SerializeField]
+    private PartySummaryDisplay partySummaryPrefab;
+
+    [SerializeField]
+    private PartyData[] parties;
+
+    private void Start()
+    {
+        var transform = this.transform;
+        foreach (var p in parties)
+        {
+            var summary = GameObject.Instantiate<PartySummaryDisplay>(partySummaryPrefab, transform);
+            summary.DisplayParty(p.Party);
+        }
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+    }
+}
