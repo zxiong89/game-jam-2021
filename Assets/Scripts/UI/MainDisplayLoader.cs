f063f180130f131c9ee0eee4279d53a0cf031f24
NullInference <nullinference@yahoo.com>
2021-07-05 19:23:05 -0700

    Create a loader for the game menu

---
+++ Assets/Scripts/UI/MainDisplayLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDisplayLoader : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject globals;
+
+    public void LoadMenuOption(GameObject prefab)
+    {
+        ClearDisplay();
+        GameObject.Instantiate(prefab, transform);
+    }
+
+    private void ClearDisplay()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            var curChild = transform.GetChild(i);
+            Destroy(curChild.gameObject);
+        }
+    }
+}
