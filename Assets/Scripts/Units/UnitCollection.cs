00c35f33dbcca8d20e7a838c20a840e34f34d4a9
NullInference <nullinference@yahoo.com>
2021-07-07 17:02:04 -0700

    Make a collection to store all active units

---
+++ Assets/Scripts/Units/UnitCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Units/UnitCollection")]
+public class UnitCollection : ScriptableObject
+{
+    public List<Unit> Units { get; } = new List<Unit>();
+}
