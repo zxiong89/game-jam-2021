c7211b6cbee69b16dd88bf57254f9804b62e2f79
zxiong89 <zxiong89@gmail.com>
2021-07-08 11:50:03 -0500

    Allow QuestCollection to be created

--- Assets/Scripts/WorldMap/Quests/QuestCollection.cs
+++ Assets/Scripts/WorldMap/Quests/QuestCollection.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(menuName = "WorldMap/QuestCollection")]
 public class QuestCollection : ScriptableObject
 {
     public List<Quest> Quests = new List<Quest>();
