950fa9144ab1bcf127eb36532a59b0264a9860ef
NullInference <nullinference@yahoo.com>
2021-07-08 18:35:17 -0700

    Highlight current speed button

--- Assets/Scripts/UI/GameSpeedController.cs
+++ Assets/Scripts/UI/GameSpeedController.cs
@@ -1,12 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSpeedController : MonoBehaviour
 {
     [SerializeField]
     private FloatEvent gameSpeedEvent;
 
-    public void SetGameSpeed(float speed)
+    [SerializeField]
+    private Image[] speedButtonImages;
+
+    [SerializeField]
+    private int[] speedButtonSpeeds;
+
+    public void SetGameSpeed(int buttonIndex)
     {
-        gameSpeedEvent.Raise(speed);
+        UpdateButtonColors(buttonIndex);
+        gameSpeedEvent.Raise(speedButtonSpeeds[buttonIndex]);
+    }
+
+    private void UpdateButtonColors(int buttonIndex)
+    {
+        for (int i = 0; i < speedButtonImages.Length; i++)
+        {
+            var buttonColor = speedButtonImages[i].color;
+            buttonColor.a = (i == buttonIndex) ? 1f : .5f;
+            speedButtonImages[i].color = buttonColor;
+        }
     }
 }
