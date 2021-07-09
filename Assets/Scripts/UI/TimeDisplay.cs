fb14cf4c60047526c6ae462185123fffb2c1e9f1
NullInference <nullinference@yahoo.com>
2021-07-08 19:52:19 -0700

    Making time display update

---
+++ Assets/Scripts/UI/TimeDisplay.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimeDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI display;
+
+    [SerializeField]
+    private int secondsPerYear = 20;
+
+    //Need to initialize this if we do save/load
+    private float currentTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        display.text = DisplayTime(currentTime);
+    }
+
+    private string DisplayTime(float time)
+    {
+        return DisplayYears(time) + " " + DisplayMonths(time);
+    }
+
+    private string DisplayYears(float time)
+    {
+        int years =  Mathf.FloorToInt(time / secondsPerYear);
+        return years.ToString() + " yr";
+    }
+
+    private string DisplayMonths(float time)
+    {
+        int months = Mathf.FloorToInt((time % secondsPerYear) / secondsPerYear * 12);
+        return months + " mo";
+    }
+}
