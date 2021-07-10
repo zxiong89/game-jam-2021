ccb45aad674e41f6439918351800b960bc1942cc
zxiong89 <zxiong89@gmail.com>
2021-07-10 14:51:59 -0500

    Add in Saving/Exiting to Settings

---
+++ Assets/Scripts/UI/Core/SettingsPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsPanel : MonoBehaviour
+{
+    public void SaveGame()
+    {
+
+    }
+
+    public void SaveAndExit()
+    {
+        SaveGame();
+        ExitGame();
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
