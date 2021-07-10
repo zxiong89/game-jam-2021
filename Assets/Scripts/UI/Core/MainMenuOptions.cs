01a8a1260a915cd9750c2003970d83f9da733346
zxiong89 <zxiong89@gmail.com>
2021-07-10 12:04:02 -0500

    Rearrange and MainMenuOptions skeleton

---
+++ Assets/Scripts/UI/Core/MainMenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuOptions : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+
+    }
+
+    public void OpenLoadGame()
+    {
+
+    }
+
+    public void ExitGame() => Application.Quit();
+}
