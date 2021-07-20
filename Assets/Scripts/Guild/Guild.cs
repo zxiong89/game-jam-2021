0cf499d250beaa1a0d1f1588ff8de080c166c02a
zxiong89 <zxiong89@gmail.com>
2021-07-20 14:37:21 -0500

    New Guild Events for EXP and Levels

--- Assets/Scripts/Guild/Guild.cs
+++ Assets/Scripts/Guild/Guild.cs
@@ -26,8 +26,61 @@ public class Guild : ScriptableObject
     }
     #endregion
 
+    #region Exp and Level
+    [Header("Exp and Level")]
+
+    [SerializeField]
+    private FloatEvent expChangedEvent;
+
+    [SerializeField]
+    private IntEvent levelChangedEvent;
+
+    [SerializeField]
+    private FloatVariable exp;
+    public float Exp
+    {
+        get { return exp.Value; }
+        set
+        {
+            exp.Value = value;
+            expChangedEvent.Raise(exp.Value);
+            levelChangedEvent.Raise(level);
+        }
+    }
+
+    private float lastExpLevel;
+    private int level;
+    public int Level
+    {
+        get
+        {
+            if (lastExpLevel != exp.Value)
+            {
+                lastExpLevel = exp.Value;
+                level = setLevel(exp.Value);
+            }
+            return level;
+        }
+    }
+
+    private int setLevel(float exp)
+    {
+        if (exp < 10e27f) return 10;
+        if (exp < 10e24f) return 9;
+        if (exp < 10e21f) return 8;
+        if (exp < 10e18f) return 7;
+        if (exp < 10e15f) return 6;
+        if (exp < 10e12f) return 5;
+        if (exp < 10e9f) return 4;
+        if (exp < 10e6f) return 3;
+        if (exp < 10e3f) return 2;
+        return 1;
+    }
+    #endregion
+
     public void Initialize()
     {
         goldChangedEvent.Raise(gold.Value);
+        expChangedEvent.Raise(exp.Value);
     }
 }
