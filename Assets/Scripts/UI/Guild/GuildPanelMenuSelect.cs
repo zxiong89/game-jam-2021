097b20cb41244a6c61850fbb020c425577b35801
zxiong89 <zxiong89@gmail.com>
2021-07-23 11:27:25 -0500

    Add in GuildPanel Menu Select

---
+++ Assets/Scripts/UI/Guild/GuildPanelMenuSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuildPanelMenuSelect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject guildInfoPanelPrefab;
+
+    [SerializeField]
+    private GameObject guildShopPanelPrefab;
+
+    public void OpenGuildInfo()
+    {
+        if (guildInfoPanelPrefab.activeInHierarchy) return;
+        guildInfoPanelPrefab.SetActive(true);
+        guildShopPanelPrefab.SetActive(false);
+    }
+
+    public void OpenGuildShop()
+    {
+        if (guildShopPanelPrefab.activeInHierarchy) return;
+        guildShopPanelPrefab.SetActive(true);
+        guildInfoPanelPrefab.SetActive(false);
+    }
+}
