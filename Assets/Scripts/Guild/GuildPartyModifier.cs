fb14f62ffaa7d5d85a6996fc8520d189a4fba177
zxiong89 <zxiong89@gmail.com>
2021-07-22 20:06:49 -0500

    Create basic Guild Party Modifiers
    10% increases for Phy Atk, Mag Atk, Def, EXP, and Gold

---
+++ Assets/Scripts/Guild/GuildPartyModifier.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Guild/PartyModifiers")]
+public class GuildPartyModifier : ScriptableObject
+{
+    public string Name;
+    public PartyStats Modifiers;
+    public float ExpModifier;
+    public float GoldModifier;
+}
---
+++ Assets/Scripts/Guild/GuildPartyModifierCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Guild/PartyModifierCollection")]
+public class GuildPartyModifierCollection : ScriptableObject
+{
+    public List<GuildPartyModifier> Modifiers = new List<GuildPartyModifier>();
+}
