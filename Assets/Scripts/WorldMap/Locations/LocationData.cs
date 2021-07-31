e82ee56f4c23bce6102a837cd3a105223a658c29
zxiong89 <zxiong89@gmail.com>
2021-07-30 23:29:21 -0500

    Tier 3 Combat Data

--- Assets/Scripts/WorldMap/Locations/LocationData.cs
+++ Assets/Scripts/WorldMap/Locations/LocationData.cs
@@ -91,21 +91,21 @@ public class LocationData : ScriptableObject
     {
         var sb = new StringBuilder();
         sb.Append(description);
+        bool maxTier = tier >= GameConstants.MaxQuestScoutingTiers;
+
+        if (tier > 1)
+        {
+            sb.AppendLine();
+            sb.Append($"Combat/Exploration Ratio: 1:{FloatExtensions.ToString(MaxEncounterRange / combatEncounterRate)}");
+        }
 
         if (tier == 2)
         {
-
-            sb.AppendLine();
-            sb.Append($"Combat/Exploration Ratio: 1:{FloatExtensions.ToString(MaxEncounterRange / combatEncounterRate)}");
-            sb.AppendLine();
             addTier2CombatData(sb);
-            sb.AppendLine();
             addTier2ExplorationData(sb);
         }
-        
-        if (tier >= GameConstants.MaxQuestScoutingTiers)
+        else if (maxTier)
         {
-            sb.AppendLine();
             addTier3CombatData(sb);
             addTier3ExplorationData(sb);
         }
@@ -123,6 +123,8 @@ public class LocationData : ScriptableObject
 
         foreach (var data in combatEncounters)
         {
+            if (data.Type.IsBoss) continue;
+            
             minAtks.Add(data.Type.Atk.Min);
             maxAtks.Add(data.Type.Atk.Max);
 
@@ -130,8 +132,8 @@ public class LocationData : ScriptableObject
             maxDefs.Add(data.Type.Def.Max);
         }
 
-        sb.Append($"Monster Attack Range: {FloatExtensions.ToString(Mathf.Min(minAtks.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(maxAtks.ToArray()))}");
-        sb.Append($"Monster Defense Range: {FloatExtensions.ToString(Mathf.Min(minDefs.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(maxDefs.ToArray()))}");
+        sb.AppendLine($"Scouted Creature Attack Range: {FloatExtensions.ToString(Mathf.Min(minAtks.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(maxAtks.ToArray()))}");
+        sb.AppendLine($"Scouted Creature Defense Range: {FloatExtensions.ToString(Mathf.Min(minDefs.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(maxDefs.ToArray()))}");
     }
 
     private void addTier2ExplorationData(StringBuilder sb)
@@ -141,20 +143,76 @@ public class LocationData : ScriptableObject
 
         foreach (var data in explorationEncounters)
         {
+            if (data.Type.IsSpecial) continue;
+
             min.Add(data.Type.Time.Min);
             max.Add(data.Type.Time.Max);
         }
 
-        sb.Append($"Exploration Time Range: {FloatExtensions.ToString(Mathf.Min(min.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(max.ToArray()))}");
+        sb.AppendLine($"Scouted Exploration Time Range: {FloatExtensions.ToString(Mathf.Min(min.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(max.ToArray()))}");
     }
 
     private void addTier3CombatData(StringBuilder sb)
     {
+        List<float> normalMinAtks = new List<float>();
+        List<float> normalMaxAtks = new List<float>();
+        List<float> normalMinDefs = new List<float>();
+        List<float> normalMaxDefs = new List<float>();
 
+        List<float> bossMinAtks = new List<float>();
+        List<float> bossMaxAtks = new List<float>();
+        List<float> bossMinDefs = new List<float>();
+        List<float> bossMaxDefs = new List<float>();
+
+        float maxSpawn = 0f;
+        float normalSpawn = 0f;
+
+        foreach (var data in combatEncounters)
+        {
+            maxSpawn += data.SpawnRate;
+            if (data.Type.IsBoss)
+            {
+                bossMinAtks.Add(data.Type.Atk.Min);
+                bossMaxAtks.Add(data.Type.Atk.Max);
+
+                bossMinDefs.Add(data.Type.Def.Min);
+                bossMaxDefs.Add(data.Type.Def.Max);
+            }
+            else
+            {
+                normalSpawn += data.SpawnRate;
+                normalMinAtks.Add(data.Type.Atk.Min);
+                normalMaxAtks.Add(data.Type.Atk.Max);
+
+                normalMinDefs.Add(data.Type.Def.Min);
+                normalMaxDefs.Add(data.Type.Def.Max);
+            }
+        }
+
+        sb.AppendLine($"There is a {FloatExtensions.ToString(normalSpawn / maxSpawn * 100)} chance to encounter non-boss creatures.");
+
+        sb.ToString();
+        sb.AppendLine($"Scouted Non-Boss Attack Range: {FloatExtensions.ToString(Mathf.Min(normalMinAtks.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(normalMaxAtks.ToArray()))}");
+        sb.AppendLine($"Scouted Non-Boss Defense Range: {FloatExtensions.ToString(Mathf.Min(normalMinDefs.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(normalMaxDefs.ToArray()))}");
+
+        sb.ToString();
+        sb.AppendLine($"Scouted Boss Attack Range: {FloatExtensions.ToString(Mathf.Min(bossMinAtks.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(bossMaxAtks.ToArray()))}");
+        sb.AppendLine($"Scouted Boss Defense Range: {FloatExtensions.ToString(Mathf.Min(bossMinDefs.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(bossMaxDefs.ToArray()))}");
     }
 
     private void addTier3ExplorationData(StringBuilder sb)
     {
+        List<float> min = new List<float>();
+        List<float> max = new List<float>();
 
+        foreach (var data in explorationEncounters)
+        {
+            if (data.Type.IsSpecial) continue;
+
+            min.Add(data.Type.Time.Min);
+            max.Add(data.Type.Time.Max);
+        }
+
+        sb.Append($"Scouted Exploration Time Range: {FloatExtensions.ToString(Mathf.Min(min.ToArray()))} - {FloatExtensions.ToString(Mathf.Max(max.ToArray()))}");
     }
 }
