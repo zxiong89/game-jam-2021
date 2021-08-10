dcd4eb7a1a40eb8b5436a4498c3e1ca8abdc9791
NullInference <nullinference@yahoo.com>
2021-08-09 19:25:24 -0700

    Buff/debuff per party class

---
+++ Assets/Scripts/Units/Traits/BuffStatPerClass.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Traits/PartyEffects/BuffStatPerClass")]
+public class BuffStatPerClass : PartyTraitEffect
+{
+    public override System.Type GetArgType()
+    {
+        return typeof(BuffStatPerClassArgs);
+    }
+
+    public override BaseTraitEffectArgs GetNewArg()
+    {
+        return new BuffStatPerClassArgs();
+    }
+
+    protected override void ApplyEffect(BaseTraitEffectArgs uncastArgs)
+    {
+        BuffStatPerClassArgs args = uncastArgs as BuffStatPerClassArgs;
+        args.CurrentParty.ApplyToParty((unit, isFront) => {
+            if(MeetsCondition(unit, args)){
+                args.CurrentUnit.Stats.GetStat(args.StatToIncrease).partyModifier += args.IncreaseAmount;
+            }
+        });
+    }
+
+    protected override void RemoveEffect(BaseTraitEffectArgs uncastArgs)
+    {
+        BuffStatPerClassArgs args = uncastArgs as BuffStatPerClassArgs;
+        foreach (var unit in args.CurrentParty.FrontLine)
+        {
+            if (MeetsCondition(unit, args))
+            {
+                args.CurrentUnit.Stats.GetStat(args.StatToIncrease).passiveModifier -= args.IncreaseAmount;
+            }
+        }
+        foreach (var unit in args.CurrentParty.BackLine)
+        {
+            if (MeetsCondition(unit, args))
+            {
+                args.CurrentUnit.Stats.GetStat(args.StatToIncrease).passiveModifier -= args.IncreaseAmount;
+            }
+        }
+    }
+    
+    private bool MeetsCondition(Unit unit, BuffStatPerClassArgs args)
+    {
+        return unit != args.CurrentUnit && unit.Class.Data.PrimaryStat == args.ClassPrimaryStat;
+    }
+}
---
+++ Assets/Scripts/Units/Traits/BuffStatPerClassArgs.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class BuffStatPerClassArgs : PartyTraitEffectArgs
+{
+    [SerializeField]
+    private StatType classPrimaryStat;
+
+    public StatType ClassPrimaryStat
+    {
+        get { return classPrimaryStat; }
+    }
+
+
+    [SerializeField]
+    private StatType statToIncrease;
+    public StatType StatToIncrease
+    {
+        get { return statToIncrease; }
+    }
+
+    [SerializeField]
+    private float increaseAmount;
+
+    public float IncreaseAmount
+    {
+        get { return increaseAmount; }
+    }
+}
