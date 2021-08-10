f21fbb840a43b2e9f3334ff4faf3f5351f4a9213
NullInference <nullinference@yahoo.com>
2021-08-09 19:23:31 -0700

    Party buffs/debuffs

---
+++ Assets/Scripts/Units/Traits/BuffPartyStat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Traits/PartyEffects/BuffPartyStat")]
+public class BuffPartyStat : PartyTraitEffect
+{
+    public override Type GetArgType()
+    {
+        return typeof(BuffPartyStatArgs);
+    }
+
+    public override BaseTraitEffectArgs GetNewArg()
+    {
+        return new BuffPartyStatArgs();
+    }
+
+    protected override void ApplyEffect(BaseTraitEffectArgs uncastArgs)
+    {
+        BuffPartyStatArgs args = uncastArgs as BuffPartyStatArgs;
+        args.CurrentParty.ApplyToParty((unit, isFront) =>
+        {
+            if(unit != args.CurrentUnit)
+            {
+                unit.Stats.GetStat(args.StatToIncrease).partyModifier += args.Amount;
+            }
+        });
+    }
+
+    protected override void RemoveEffect(BaseTraitEffectArgs uncastArgs)
+    {
+        BuffPartyStatArgs args = uncastArgs as BuffPartyStatArgs;
+        args.CurrentParty.ApplyToParty((unit, isFront) =>
+        {
+            if(unit != args.CurrentUnit)
+            {
+                unit.Stats.GetStat(args.StatToIncrease).partyModifier -= args.Amount;
+            }
+        });
+    }
+}
---
+++ Assets/Scripts/Units/Traits/BuffPartyStatArgs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffPartyStatArgs : PartyTraitEffectArgs
+{
+    [SerializeField]
+    private StatType statToIncrease;
+
+    public StatType StatToIncrease
+    {
+        get { return statToIncrease; }
+    }
+
+
+    [SerializeField]
+    private float amount;
+
+    public float Amount
+    {
+        get { return amount; }
+    }
+
+}
