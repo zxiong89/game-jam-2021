1ecdd95d72bc8a79aada50776a75126b0d575a9d
NullInference <nullinference@yahoo.com>
2021-08-09 19:22:00 -0700

    Passive buffs/debuffs

---
+++ Assets/Scripts/Units/Traits/PassiveStatBuff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Traits/PassiveEffects/PassiveStatBuff")]
+[System.Serializable]
+public class PassiveStatBuff : PassiveTraitEffect
+{
+    public override Type GetArgType()
+    {
+        return typeof(PassiveStatBuffArgs);
+    }
+
+    public override BaseTraitEffectArgs GetNewArg()
+    {
+        return new PassiveStatBuffArgs();
+    }
+
+    protected override void ApplyEffect(BaseTraitEffectArgs uncastArgs)
+    {
+        var args = uncastArgs as PassiveStatBuffArgs;
+        if (args == null) return;
+        args.CurrentUnit.Stats.GetStat(args.StatToIncrease).passiveModifier += args.Amount;
+    }
+
+    protected override void RemoveEffect(BaseTraitEffectArgs uncastArgs)
+    {
+        var args = uncastArgs as PassiveStatBuffArgs;
+        if (args == null) return;
+        args.CurrentUnit.Stats.GetStat(args.StatToIncrease).passiveModifier -= args.Amount;
+    }
+}
