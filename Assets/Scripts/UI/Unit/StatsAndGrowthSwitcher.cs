c1ecc41107d0f3d2f17d1f2a944c1c260030e0a3
zxiong89 <zxiong89@gmail.com>
2021-08-24 23:51:03 -0500

    Add base for Switching between Stats and Growth

---
+++ Assets/Scripts/UI/Unit/StatsAndGrowthSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsAndGrowthSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private UnitDisplay unitDisplay;
+
+    [SerializeField]
+    private Button showStatsBtn;
+
+    [SerializeField]
+    private Button showGrowthBtn;
+
+    private void Start()
+    {
+        showStatsBtn.onClick.AddListener(() => ShowStats());
+        showGrowthBtn.onClick.AddListener(() => ShowGrowth());
+    }
+
+    public void ShowStats()
+    {
+        activateButtons(false);
+    }
+
+    public void ShowGrowth()
+    {
+        activateButtons(true);
+    }
+
+    private void activateButtons(bool showStats)
+    {
+        showStatsBtn.gameObject.SetActive(showStats);
+        showGrowthBtn.gameObject.SetActive(!showStats);
+    }
+}
