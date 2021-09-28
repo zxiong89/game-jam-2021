5a2908058d376856e6f1ae3abc121c88ccb74780
NullInference <nullinference@yahoo.com>
2021-09-28 01:36:56 -0700

    Event for yearly tax day

---
+++ Assets/Scripts/Simulation/YearlyEvents/TaxDay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TaxDay : BaseYearlyEvent
+{
+    public Guild PlayerGuild { get; set; }
+
+    public override void RunEvent()
+    {
+        int taxes = Mathf.FloorToInt(Mathf.Pow((PlayerGuild.Level * 10), 2));
+        EventLog.AddMessage("It's tax day! Paid " + taxes + "G in yearly guild taxes");
+        PlayerGuild.Gold -= taxes;        
+    }
+}
---
+++ Assets/Scripts/Simulation/YearlyEvents/TaxDayParams.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "YearlyEvents/TaxDayParams")]
+public class TaxDayParams : BaseYearlyEventParams<TaxDay>
+{
+    [SerializeField]
+    private Guild playerGuild;
+
+    public override TaxDay CreateInitialEventHelper()
+    {
+        var taxDay = base.CreateInitialEventHelper();
+        taxDay.PlayerGuild = playerGuild;
+        return taxDay;
+    }
+}
